a6f8857a98293d2f5eea79d1246020efbcd1df52
leVirve <gae.m.project@gmail.com>
2015-10-25 23:07:16 +0800

    add timeit function

--- src/advanced.c
+++ src/advanced.c
@@ -1,4 +1,5 @@
 #include <mpi.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,7 +17,6 @@
 #define bool        char
 #define true        1
 #define false       0
-#define MICRO_SEC   1000000
 #define channel1    0
 #define channel2    1
 #define LEFT_PHASE  0
@@ -24,9 +24,20 @@
 #define is_odd(x)   ((x) & 1)
 #define is_even(x)  (!is_odd(x))
 #define swap(i, j)  int t = i; i = j; j = t;
+#define timeit(t)   clock_gettime(CLOCK_REALTIME, t)
 
 bool sorted = false;
-int world_size, world_rank, subset_size;
+int world_size, world_rank, subset_size, *nums, *recv, *tmp;
+
+long io_time = 0, comp_time = 0, comm_time = 0;
+struct timespec s, e;
+
+void calc_time(long* target, struct timespec a, struct timespec b)
+{
+    int sec = a.tv_sec - b.tv_sec;
+    int nsec = a.tv_nsec - b.tv_nsec;
+    *target += (long)sec * 1000000000 + nsec;
+}
 
 int cmp(const void* a, const void* b)
 {
@@ -43,9 +54,12 @@ void mpi_read_file(char* filename, int* nums, int* count)
                   MPI_MODE_RDONLY, MPI_INFO_NULL, &input);
     MPI_File_set_view(input, sizeof(int) * subset_size * world_rank,
                       MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
+    timeit(&s);
     MPI_File_read_all(input, nums, subset_size, MPI_INT, &status);
+    timeit(&e); calc_time(&io_time, e, s);
     MPI_File_close(&input);
     MPI_Get_count(&status, MPI_INT, count);
+    DEBUG("My rank=%d/%d read_size=%d\n", world_rank, world_size, *count);
 }
 
 void mpi_write_file(char* filename, int* nums, int* count)
@@ -56,14 +70,16 @@ void mpi_write_file(char* filename, int* nums, int* count)
                   MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &fh);
     MPI_File_set_view(fh, sizeof(int) * subset_size * world_rank,
                       MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
+    timeit(&s);
     MPI_File_write_all(fh, nums, *count, MPI_INT, &status);
+    timeit(&e); calc_time(&io_time, e, s);
     MPI_File_close(&fh);
 }
 
 void _merge(int* buf, int lsz, int* recv, int rsz, bool phase)
 {
-    int tmp[subset_size * 2];
     int i = 0, j = 0, k = 0, memsz, *memptr;
+    timeit(&s);
     while (i < lsz || j < rsz) {
         if (i >= lsz) tmp[k++] = recv[j++];
         else if (j >= rsz) tmp[k++] = buf[i++];
@@ -73,15 +89,18 @@ void _merge(int* buf, int lsz, int* recv, int rsz, bool phase)
     memptr = (phase == LEFT_PHASE) ? tmp : tmp + rsz;
     if (memcmp(buf, memptr, memsz) != 0) sorted = false;
     memcpy(buf, memptr, memsz);
+    timeit(&e); calc_time(&comp_time, e, s);
 }
 
 void mpi_recv(int rank, int* nums, int count)
 {
     if (world_rank >= world_size || rank < 0) return;
-    int recv[subset_size], c;
+    int c;
     MPI_Status stat;
+    timeit(&s);
     MPI_Recv(recv, subset_size, MPI_INT, rank, channel1, MPI_COMM_WORLD, &stat);
     MPI_Send(nums, count, MPI_INT, rank, channel2, MPI_COMM_WORLD);
+    timeit(&e); calc_time(&comm_time, e, s);
     MPI_Get_count(&stat, MPI_INT, &c);
     _merge(nums, count, recv, c, RIGHT_PHASE);
 }
@@ -89,17 +108,19 @@ void mpi_recv(int rank, int* nums, int count)
 void mpi_send(int rank, int* nums, int count)
 {
     if (rank >= world_size) return;
-    int recv[subset_size], c;
+    int c;
     MPI_Status stat;
+    timeit(&s);
     MPI_Send(nums, count, MPI_INT, rank, channel1, MPI_COMM_WORLD);
     MPI_Recv(recv, subset_size, MPI_INT, rank, channel2, MPI_COMM_WORLD, &stat);
+    timeit(&e); calc_time(&comm_time, e, s);
     MPI_Get_count(&stat, MPI_INT, &c);
     _merge(nums, count, recv, c, LEFT_PHASE);
 }
 
 int main(int argc, char** argv)
 {
-    int file_size, count, *nums;
+    int file_size, count;
     sscanf(argv[1], "%d", &file_size);
 
     MPI_Init(&argc, &argv);
@@ -113,11 +134,18 @@ int main(int argc, char** argv)
     if (world_size <= 1) sorted = true;
 
     nums = (int*) malloc(subset_size * sizeof(int));
+    recv = (int*) malloc(subset_size * sizeof(int));
+    tmp = (int*) malloc(subset_size * 2 * sizeof(int));
     mpi_read_file(argv[2], nums, &count);
+
+    timeit(&s);
     qsort(nums, count, sizeof(int), cmp);
+    timeit(&e); calc_time(&comp_time, e, s);
+#ifdef _TIME
+    printf("world_size %d\n", world_size);
+#endif
     while (!sorted) {
         sorted = true;
-
         /*** even-phase ***/
         if (is_even(world_rank)) mpi_send(world_rank + 1, nums, count);
         else mpi_recv(world_rank - 1, nums, count);
@@ -129,10 +157,22 @@ int main(int argc, char** argv)
         MPI_Barrier(MPI_COMM_WORLD);
 
         bool tmp = sorted;
+        timeit(&s);
         MPI_Allreduce(&tmp, &sorted, 1, MPI_CHAR, MPI_BAND, MPI_COMM_WORLD);
+        timeit(&e); calc_time(&comm_time, e, s);
     }
     mpi_write_file(argv[3], nums, &count);
     MPI_Finalize();
     free(nums);
+    free(recv);
+    free(tmp);
+
+#ifdef _TIME
+    printf("#Rank%d: total=%ld nsec (io=%ld, comm=%ld, comp=%ld)\n",
+           world_rank,
+           io_time + comm_time + comp_time,
+           io_time, comm_time, comp_time);
+#endif
+
     return 0;
 }
--- src/basic.c
+++ src/basic.c
@@ -1,4 +1,5 @@
 #include <mpi.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,7 +17,6 @@
 #define bool        char
 #define true        1
 #define false       0
-#define MICRO_SEC   1000000
 #define channel1    0
 #define channel2    1
 #define EVEN_PHASE  0
@@ -25,21 +25,34 @@
 #define is_odd(x)   ((x) & 1)
 #define is_even(x)  (!is_odd(x))
 #define swap(i, j)  int t = i; i = j; j = t;
+#define timeit(t)   clock_gettime(CLOCK_REALTIME, t)
 
 bool sorted = false;
 int world_size, world_rank, subset_size;
+
+long io_time = 0, comp_time = 0, comm_time = 0;
+struct timespec s, e;
+
+void calc_time(long* target, struct timespec a, struct timespec b)
+{
+    int sec = a.tv_sec - b.tv_sec;
+    int nsec = a.tv_nsec - b.tv_nsec;
+    *target += sec * 1000000000 + nsec;
+}
 
 void mpi_read_file(char* filename, int* nums, int* count)
 {
     MPI_File input;
     MPI_Status status;
 
+    timeit(&s);
     MPI_File_open(MPI_COMM_WORLD, filename,
                   MPI_MODE_RDONLY, MPI_INFO_NULL, &input);
     MPI_File_set_view(input, sizeof(int) * subset_size * world_rank,
                       MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
     MPI_File_read_all(input, nums, subset_size, MPI_INT, &status);
     MPI_File_close(&input);
+    timeit(&e); calc_time(&io_time, e, s);
     MPI_Get_count(&status, MPI_INT, count);
     DEBUG("My rank=%d/%d read_size=%d\n", world_rank, world_size, *count);
 }
@@ -49,12 +62,14 @@ void mpi_write_file(char* filename, int* nums, int* count)
     MPI_File fh;
     MPI_Status status;
 
+    timeit(&s);
     MPI_File_open(MPI_COMM_WORLD, filename,
                   MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &fh);
     MPI_File_set_view(fh, sizeof(int) * subset_size * world_rank,
                       MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
     MPI_File_write_all(fh, nums, *count, MPI_INT, &status);
     MPI_File_close(&fh);
+    timeit(&e); calc_time(&io_time, e, s);
 }
 
 void mpi_recv(int rank, int* nums)
@@ -62,9 +77,15 @@ void mpi_recv(int rank, int* nums)
     if (world_rank >= world_size || rank < 0) return;
     int carrier;
     MPI_Status status;
+    timeit(&s);
     MPI_Recv(&carrier, 1, MPI_INT, rank, channel1, MPI_COMM_WORLD, &status);
+    timeit(&e); calc_time(&comm_time, e, s);
+    timeit(&s);
     if (carrier > nums[0]) { swap(carrier, nums[0]); sorted = false; }
+    timeit(&e); calc_time(&comp_time, e, s);
+    timeit(&s);
     MPI_Send(&carrier, 1, MPI_INT, rank, channel2, MPI_COMM_WORLD);
+    timeit(&e); calc_time(&comm_time, e, s);
 }
 
 void mpi_send(int rank, int* nums, int count)
@@ -72,16 +93,20 @@ void mpi_send(int rank, int* nums, int count)
     if (rank >= world_size) return;
     int carrier;
     MPI_Status status;
+    timeit(&s);
     MPI_Send(&nums[count - 1], 1, MPI_INT, rank, channel1, MPI_COMM_WORLD);
     MPI_Recv(&carrier, 1, MPI_INT, rank, channel2, MPI_COMM_WORLD, &status);
+    timeit(&e); calc_time(&comm_time, e, s);
     nums[count - 1] = carrier;
 }
 
 void _single_phase_sort(int* a, int index, int size)
 {
     int i = index;
+    timeit(&s);
     for (; i < size - 1; i += 2)
         if (a[i] > a[i + 1]) { swap(a[i], a[i + 1]); sorted = false; }
+    timeit(&e); calc_time(&comp_time, e, s);
 }
 
 int main(int argc, char** argv)
@@ -107,7 +132,9 @@ int main(int argc, char** argv)
     mpi_read_file(argv[2], nums, &count);
 
     DEBUG("#%d/%d count=%d\n", world_rank, world_size, count);
-
+#ifdef _TIME
+    printf("world_size %d\n", world_size);
+#endif
     while (!sorted) {
         sorted = true;
 
@@ -137,14 +164,24 @@ int main(int argc, char** argv)
 
         if (!single_process) {
             bool tmp = sorted;
+            timeit(&s);
             MPI_Allreduce(&tmp, &sorted, 1, MPI_CHAR, MPI_BAND, MPI_COMM_WORLD);
+            timeit(&e); calc_time(&comm_time, e, s);
         }
     }
 
     mpi_write_file(argv[3], nums, &count);
     free(nums);
 
-    INFO("#%d leave sorting-loop(%d)\n", world_rank, count);
+    DEBUG("#%d leave sorting-loop(%d)\n", world_rank, count);
     MPI_Finalize();
+
+#ifdef _TIME
+    printf("#Rank%d: total=%ld nsec (io=%ld, comm=%ld, comp=%ld)\n",
+           world_rank,
+           io_time + comm_time + comp_time,
+           io_time, comm_time, comp_time);
+#endif
+
     return 0;
 }
