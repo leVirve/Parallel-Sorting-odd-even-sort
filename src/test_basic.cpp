32a6fc3a29a506d2641da0a8f50c87978d926495
leVirve <gae.m.project@gmail.com>
2015-10-25 19:44:54 +0800

    add testing file

---
+++ src/test_basic.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <ctime>
+#include <algorithm>
+#define timeit(t)   clock_gettime(CLOCK_REALTIME, t)
+using namespace std;
+
+int file_size, *a;
+
+long io_time = 0, comp_time = 0, comm_time = 0;
+struct timespec s, e;
+
+void calc_time(long* target, struct timespec a, struct timespec b)
+{
+    int sec = a.tv_sec - b.tv_sec;
+    int nsec = a.tv_nsec - b.tv_nsec;
+    *target += (long)sec * 1000000000 + nsec;
+}
+
+int cmp(const void* a, const void* b)
+{
+    /* watch out! it may overflow with return x - y */
+    int x = *(int*)a, y = *(int*)b;
+    return (x > y) - (x < y);
+}
+
+int main(int argc, char const *argv[])
+{
+    sscanf(argv[1], "%d", &file_size);
+    FILE* fp = fopen(argv[2], "rb");
+    a = (int*) malloc(sizeof(int) * file_size);
+
+    timeit(&s);
+    fread(a, sizeof(int), file_size, fp);
+    timeit(&e); calc_time(&io_time, e, s);
+    fclose(fp);
+
+    bool sorted = false;
+    timeit(&s);
+    // sort(a, a + file_size);
+    qsort(a, file_size, sizeof(int), cmp);
+    timeit(&e); calc_time(&comp_time, e, s);
+
+    FILE* fh = fopen(argv[3], "wb");
+    timeit(&s);
+    fwrite(a, sizeof(int), file_size, fh);
+    timeit(&e); calc_time(&io_time, e, s);
+    fclose(fh);
+
+    printf("%ld %ld\n", io_time, comp_time);
+    return 0;
+}
